b94402f1eed6e0e4190a08653ea449c8277e3e83
pjooklas <pjooklas@users.noreply.github.com>
2022-07-17 17:54:50 +0300

    timer operator

--- src/index.js
+++ src/index.js
@@ -1,36 +1,9 @@
-import { Observable } from "rxjs";
+import { timer } from "rxjs";
 
-const observable = new Observable((subscriber) => {
-    // subscriber.next('Hello world')
-    // subscriber.error('Error')
-    // subscriber.complete()
-    // subscriber.next('nxt')
-    const id = setInterval(() => {
-        subscriber.next('test')
-        console.log('leak');
-    }, 1000)
+const observable = timer(0, 1000);
 
-    return () => {
-        clearInterval(id)
-    }
-});
+const subscirption = observable.subscribe(
+    console.log
+)
 
-console.log('before');
 
-const subscirption = observable.subscribe({
-    next: (value) => {
-        console.log(value);
-    },
-    complete: () => {
-        console.log('complete');
-    },
-    error: (err) => {
-        console.error(err);
-    }
-})
-
-setTimeout(() => {
-    subscirption.unsubscribe();
-}, 4000)
-
-console.log('after');
