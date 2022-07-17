463ea8ecd682f45c0bff8525422b4ea9761ce279
pjooklas <pjooklas@users.noreply.github.com>
2022-07-17 16:40:42 +0300

    closing observable

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import { Observable } from "rxjs";
+
+const observable = new Observable((subscriber) => {
+    // subscriber.next('Hello world')
+    // subscriber.error('Error')
+    // subscriber.complete()
+    // subscriber.next('nxt')
+    const id = setInterval(() => {
+        subscriber.next('test')
+        console.log('leak');
+    }, 1000)
+
+    subscriber.complete()
+
+    return () => {
+        clearInterval(id)
+    }
+});
+
+console.log('before');
+
+observable.subscribe({
+    next: (value) => {
+        console.log(value);
+    },
+    complete: () => {
+        console.log('complete');
+    },
+    error: (err) => {
+        console.error(err);
+    }
+})
+
+console.log('after');
