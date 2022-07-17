0bdf499ce8ce468f9bcc2b09096249e361b9ecc5
pjooklas <pjooklas@users.noreply.github.com>
2022-07-17 18:37:26 +0300

    take, scan operators

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import { of } from "rxjs";
-import { reduce } from "rxjs/operators";
+import { interval } from "rxjs";
+import { reduce, take, scan } from "rxjs/operators";
 
-const observable = of(1,2,3,4,5).pipe(
-    reduce(
+const observable = interval(500).pipe(
+    take(5),
+    scan(
         (acc, val) => acc + val,
         0
     )
